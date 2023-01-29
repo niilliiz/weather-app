54d0580c79d531a2336399dc35cd568e6b8f8402
niilliiz <niloo.vatan.nv@@gmail.com>
2023-01-29 19:50:51 +0330

    feat: header navbar

--- src/sections/header/header.js
+++ src/sections/header/header.js
@@ -1,4 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import {
+  CalendarCheck,
+  Clock,
+  CalendarBlank,
+  Target,
+  MonitorPlay,
+} from "phosphor-react";
 import React, { useState } from "react";
 
 const UNIT = {
@@ -6,14 +13,47 @@ const UNIT = {
   c: "f",
 };
 
+const SHORT_SCREEN_NAV_ITEM = [
+  {
+    title: "Today",
+    icon: <CalendarCheck size={24} color="#7e7e7e" />,
+  },
+  {
+    title: "Hourly",
+    icon: <Clock size={24} color="#7e7e7e" />,
+  },
+  {
+    title: "10 Day",
+    icon: <CalendarBlank size={24} color="#7e7e7e" />,
+  },
+  {
+    title: "Radar",
+    icon: <Target size={24} color="#7e7e7e" />,
+  },
+  {
+    title: "Videos",
+    icon: <MonitorPlay size={24} color="#7e7e7e" />,
+  },
+];
+
+const LARGE_SCREEN_NAV_ITEM = [
+  "Today",
+  "Hourly",
+  "10 Day",
+  "Weekend",
+  "Monthly",
+  "Radar",
+  "Videos",
+];
+
 const Header = () => {
   const [unit, setUnit] = useState("f");
   return (
-    <header className="flex flex-col text-gray_100">
+    <header className="fixed top-0 left-0 right-0  flex flex-col ">
       <div className="bg-dark_blue">
         <div className="container flex justify-between items-center py-4 px-4">
-          <a href="#" className="w-16 text-xs bg-gray_100 text-dark_blue">
-            <span className="block mt-3 font-extrabold px-1 pb-1 leading-3">
+          <a href="#" className="block w-logo bg-gray_100 text-dark_blue ">
+            <span className="block mt-3 font-extrabold px-2 pb-2 leading-4">
               The Weather Channel
             </span>
           </a>
@@ -26,20 +66,39 @@ const Header = () => {
                 id="switch_checkbox"
                 onChange={() => setUnit(UNIT[unit])}
               />
-              <button className="ball">°{UNIT[unit].toUpperCase()}</button>
+              <button className="ball text-12 text-gray_100">
+                °{UNIT[unit].toUpperCase()}
+              </button>
             </label>
-            {/* <label className="checkbox-container">
-              <input className="checkbox" type="checkbox" />
-              <div className="ball_container">
-                <div className="ball"></div>
-              </div>
-            </label> */}
           </div>
         </div>
       </div>
-      <div className="bg-light_blue px-10 py-4">
+      <div className="hidden bg-light_blue px-10 py-4">
         <div className="container">little summary</div>
       </div>
+      <nav className="">
+        <ul className="fixed left-0 right-0 bottom-0 flex justify-between shadow-shadow-top px-4 py-2 lg:hidden">
+          {SHORT_SCREEN_NAV_ITEM.map((item) => (
+            <li
+              key={item.title}
+              className="flex flex-col space-y-1 items-center"
+            >
+              {item.icon}
+              <span className="text-xs">{item.title}</span>
+            </li>
+          ))}
+        </ul>
+        <ul className="hidden bg-light_blue text-gray_100 justify-between items-center px-4 pt-4 lg:flex">
+          {LARGE_SCREEN_NAV_ITEM.map((item) => (
+            <li
+              key={item.title}
+              className="h-10 hover:border-b-4 hover:border-gray-100"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    // fontSize: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      12: "0.75rem",
+      14: "0.875rem",
+      base: "1rem",
+      18: "1.125rem",
+      20: "1.25rem",
+      24: "1.5rem",
+    },
     colors: {
       light_blue: "#355773",
       dark_blue: "#022D50",
       gray_100: "#fff",
+      gray_300: "#7e7e7e",
       gray_900: "#333",
     },
     extend: {
       lineHeight: {
         "extra-packed": "0.5",
+      },
+      boxShadow: {
+        "shadow-top": "0 -2px 10px rgba(0, 0, 0, 0.1)",
+      },
+      width: {
+        logo: "5.5rem",
       },
     },
   },
